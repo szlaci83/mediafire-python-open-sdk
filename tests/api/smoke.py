2712e5d1ed66b31a76dc6f6f7eba0e51862dc1f2
Roman Yepishev <rye@delorean.keypressure.com>
2015-04-28 22:29:13 -0400

    Smoke test for CI environment, initial version

---
+++ tests/api/smoke.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+import unittest
+from mediafire import MediaFireApi
+
+APP_ID = '42511'
+
+MEDIAFIRE_EMAIL = os.environ['MEDIAFIRE_EMAIL']
+MEDIAFIRE_PASSWORD = os.environ['MEDIAFIRE_PASSWORD']
+
+
+@unittest.skipIf('CI' not in os.environ, "Running outside CI environment")
+class MediaFireSmokeTest(unittest.TestCase):
+    """Smoke test for API"""
+
+    def __init__(self, *args, **kwargs):
+        super(MediaFireSmokeTest, self).__init__(*args, **kwargs)
+        self.api = MediaFireApi()
+        session = self.api.user_get_session_token(
+            app_id=APP_ID, email=MEDIAFIRE_EMAIL, password=MEDIAFIRE_PASSWORD)
+        self.api.session = session
+
+    def test_user_get_info(self):
+        result = self.api.user_get_info()
+        self.assertEqual(result['user_info']['display_name'], u"Smoke Test")
+
+if __name__ == "__main__":
+    unittest.main()
