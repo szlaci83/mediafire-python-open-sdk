71c53423be8688d2b34d7282fb45c490c7715441
Roman Yepishev <rye@keypressure.com>
2016-01-06 23:26:57 -0500

    Add example of separate hashing and upload flow

---
+++ examples/hashing-upload.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+"""An example of providing hash information to the uploader
+
+You will want to use this method if the file you are uploading
+is large enough for the hashing to take considerable time.
+
+Since uploads sometimes fail, recomputing the hash on 16GiB file
+every time is not fun at all.
+
+Usage:
+
+    export MEDIAFIRE_EMAIL=email@example.com
+    export MEDIAFIRE_PASSWORD=mediafire-password
+    # get hash information
+    HASH_INFO=$(hashing-upload.py /path/to/file)
+
+    # upload with stored hash
+    hashing-upload.py /path/to/file "$HASH_INFO"
+
+
+MediaFireHashInfo fields:
+
+ * file - sha256 hexdigest of the whole file
+ * units[] - array of sha256 hexdigest of the separate file units
+ * size - size of the file at the time when hash was computed.
+          in case of mismatch, the file has definitely changed
+          and the hash_info structure is no longer valid
+
+"""
+
+
+import json
+import logging
+import os
+import sys
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+from mediafire import (MediaFireApi, MediaFireUploader)
+
+from mediafire.uploader import (compute_hash_info,
+                                compute_resumable_upload_unit_size,
+                                UPLOAD_SIMPLE_LIMIT_BYTES,
+                                MediaFireHashInfo)
+
+APP_ID = '42511'
+MEDIAFIRE_EMAIL = os.environ['MEDIAFIRE_EMAIL']
+MEDIAFIRE_PASSWORD = os.environ['MEDIAFIRE_PASSWORD']
+
+
+def hash_info_to_json(hash_info):
+    """Convert HashInfo to JSON string"""
+    return json.dumps({
+        "file": hash_info.file,
+        "units": hash_info.units,
+        "size": hash_info.size
+    })
+
+
+def json_to_hash_info(json_doc):
+    """Convert JSON string to HashInfo"""
+    hash_info_doc = json.loads(json_doc)
+
+    return MediaFireHashInfo(
+        file=hash_info_doc["file"],
+        units=hash_info_doc["units"],
+        size=hash_info_doc["size"]
+    )
+
+
+def compute_hash_and_return_json(path):
+    """Convert HashInfo to JSON string"""
+    stat_result = os.stat(path)
+    size = stat_result.st_size
+
+    if size > UPLOAD_SIMPLE_LIMIT_BYTES:
+        unit_size = compute_resumable_upload_unit_size(size)
+    else:
+        unit_size = None
+
+    with open(path, 'rb') as fd:
+        # save the hash_info somewhere on a persistent media
+        hash_info = compute_hash_info(fd, unit_size)
+
+    return hash_info_to_json(hash_info)
+
+
+def upload_with_hash_info_json(path, hash_info_json):
+
+    hash_info = json_to_hash_info(hash_info_json)
+
+    api = MediaFireApi()
+
+    session = api.user_get_session_token(
+        app_id=APP_ID, email=MEDIAFIRE_EMAIL,
+        password=MEDIAFIRE_PASSWORD
+    )
+
+    api.session = session
+
+    uploader = MediaFireUploader(api)
+
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    with open(path, 'rb') as fd:
+        result = uploader.upload(
+            fd, os.path.basename(path), hash_info=hash_info)
+
+    return result
+
+
+def main(path, hash_info_json):
+    if hash_info_json is None:
+        print(compute_hash_and_return_json(path))
+    else:
+        print(upload_with_hash_info_json(path, hash_info_json))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(__doc__)
+        sys.exit(1)
+
+    path = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        hash_info = sys.argv[2]
+    else:
+        hash_info = None
+
+    main(path, hash_info)
